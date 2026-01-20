cae45cffee42cd84cb7a0da5e40d1f66ff401a12
Umaiza04 <umaiza.cb2022@jerusalemengg.ac.in>
2026-01-20 11:10:01 +0530

    Initial Commit

---
+++ loop_implementation.py
@@ -0,0 +1,20 @@
+#for loop implementation
+n=11
+#print 1 to 10
+for i in range(1,n): 
+  print(i)
+
+
+#getting input from the user
+n=int(input("enter a number:"))
+for i in range(1,n):
+  print(i)
+
+
+#find odd or even using for loop
+n=9
+for i in range(1):
+  if n%2==0:
+    print("even")
+  else:
+    print("odd")
