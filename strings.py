35d3524f933165b3a47e85f423bbb4264d5abc2c
Umaiza04 <umaiza.cb2022@jerusalemengg.ac.in>
2026-01-20 15:44:13 +0530

    third commit

---
+++ strings.py
@@ -0,0 +1,25 @@
+#defining a string
+st="provility"
+print(st)
+
+#update single char from string
+str="provility"
+str1='P'+ str[1:]
+print(str1)
+
+#update the whole string
+str="provility training"
+str1=str.replace("training", "software")
+print(str1)
+
+#slicing
+str="provility"
+print(str[3:])
+
+#indexing
+str="provility"
+print(str[5])
+
+#delete a string
+str="provility"
+del str
