4e3676070cdf7c5bda5e933b11428a6977416ca5
Umaiza04 <umaiza.cb2022@jerusalemengg.ac.in>
2026-01-22 14:50:58 +0530

    third task commit

---
+++ string_task.py
@@ -0,0 +1,21 @@
+text="PythonProgramming"
+#indexing
+print(text[0])
+print(text[4])
+print(text[-2])
+
+#slicing
+print(text[:6])
+print(text[6:])
+print(text[2:9])
+print(text[1:])
+print(text[:16])
+
+
+#length of the string
+print("Length of the string:",len(text))
+
+#step slicing
+print("Printing every second letter:",text[::2])
+print("Reverse:",text[::-1])
+
