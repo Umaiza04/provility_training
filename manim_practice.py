feab58956f00df4a7fdf3d457e4f000c07fa73ca
Umaiza04 <umaiza.cb2022@jerusalemengg.ac.in>
2026-01-23 15:14:04 +0530

    manim task commit

--- manim_practice.py
+++ manim_practice.py
@@ -5,9 +5,9 @@ from numpy import right_shift
 class manim_practice(Scene):
     def construct(self):
        text=Text("ROBOGEBRA",color=BLUE,font_size=70).shift(UP*3)
-       triangle=Triangle(fill_color=PINK,fill_opacity=0.5).scale(1.5)
-       square=Square(fill_color=BLUE, fill_opacity=0.5).scale(1)
-       circle=Circle(fill_color=GREEN, fill_opacity=0.5)
+       triangle=Triangle(fill_color=PINK,fill_opacity=0.5).scale(1.5).set_stroke(width=15)
+       square=Square(fill_color=BLUE, fill_opacity=0.5).scale(1).set_stroke(width=15)
+       circle=Circle(fill_color=GREEN, fill_opacity=0.5).set_stroke(width=15)
        circle.shift(LEFT*3)
        square.shift(RIGHT*3)
        self.add(triangle)
