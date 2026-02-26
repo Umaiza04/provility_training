0ac3e84330f8bbca27c97f214756439825f924e2
Umaiza04 <umaiza.cb2022@jerusalemengg.ac.in>
2026-02-26 11:45:48 +0530

    manim task commit

--- 11th_sb_chapter8/exercise8510/step_1.py
+++ 11th_sb_chapter8/exercise8510/step_1.py
@@ -35,11 +35,12 @@ class Step_1(Scene):
         #labels
         label_A = MathTex("A",color=BLACK).next_to(A, LEFT)
         label_B = MathTex("B",color=BLACK).next_to(B, RIGHT)
-        label_C = MathTex("C",color=BLACK).next_to(P, UP*0.7)
+        label_C = MathTex("C",color=BLACK).next_to(P, UP*0.6)
         label_O = MathTex("O",color=BLACK).next_to(O, DOWN)
         label_a = MathTex(r"\vec a",color=BLACK).next_to(OA, LEFT).shift(RIGHT*1.2)
         label_b = MathTex(r"\vec b",color=BLACK).next_to(OB, RIGHT).shift(LEFT*1.2)
-        label_mn = MathTex("m : n", color="#E53935").next_to(P, UP*3.5)
+        label_mn = MathTex("m : n", color="#E53935").next_to(P, UP*4.7)
+        label = MathTex("1 : 2", color=BLACK).next_to(P, UP*2.7).shift(RIGHT*0.05)
         #curve
         curve_start = P + UP*1.4+RIGHT*0.4          
         curve_control1 = curve_start + UP*1.8
@@ -51,13 +52,13 @@ class Step_1(Scene):
             curve_control2,
             curve_end,
             color="#E53935",
-            stroke_width=3)
+            stroke_width=3).shift(UP*0.2)
         p1 = curve1.point_from_proportion(0.9)
         p2 = curve1.point_from_proportion(1.0)
         angle = np.arctan2(p2[1] - p1[1], p2[0] - p1[0])
         curve_tip = StealthTip(color="#E53935", length=0.25)
         curve_tip.rotate(angle)
-        curve_tip.move_to(curve_end)
+        curve_tip.move_to(curve_end).shift(UP*0.2)
         #curve2
         curve2_start = P + UP*1.4 + LEFT*0.4
         curve2_control1 = curve2_start + UP*1.8
@@ -69,11 +70,11 @@ class Step_1(Scene):
             curve2_control2,
             curve2_end,
             color="#E53935",
-            stroke_width=3)
+            stroke_width=3).shift(UP*0.2)
         q1 = curve2.point_from_proportion(0.9)
         q2 = curve2.point_from_proportion(1.0)
         angle2 = np.arctan2(q2[1] - q1[1], q2[0] - q1[0])
-        tip5 = StealthTip(color="#E53935", length=0.25).rotate(angle2).move_to(curve2_end)
+        tip5 = StealthTip(color="#E53935", length=0.25).rotate(angle2).move_to(curve2_end).shift(UP*0.2)
 
         self.add(OP, AB, OA, OB,tip1, tip2, tip3,label_A, label_B, label_C, label_O,label_a, label_b,
-            label_mn,curve1, curve_tip,curve2,tip4,tip5)
+            label_mn,label,curve1, curve_tip,curve2,tip4,tip5)
