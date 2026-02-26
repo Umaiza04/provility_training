c9144c3dc84b02a5f4327c746cd03a5c62d97f0f
Umaiza04 <umaiza.cb2022@jerusalemengg.ac.in>
2026-02-26 16:49:35 +0530

    manim task commit

--- 11th_sb_chapter9/example94/step_1.py
+++ 11th_sb_chapter9/example94/step_1.py
@@ -7,43 +7,42 @@ config.pixel_height=2800
 class Step_1(Scene):
     def construct(self):
         self.camera.background_color = WHITE
-        axes = Axes(
-            x_range=[-4, 4, 1],
-            y_range=[-4, 4, 1],
-            x_length=9,
-            y_length=9,
-            axis_config={"color": "#02590F",
-                          "include_tip": True,
-                          "tip_length": 0.35,
-                          "tip_width": 0.25 },
-            x_axis_config={"numbers_to_include": list(range(-3, 4)),
-                           "decimal_number_config": {
-                           "num_decimal_places": 0,  
-                           "color": BLACK, }, },
-            y_axis_config={"numbers_to_include": list(range(-3, 4)),
-                           "decimal_number_config": {
-                           "num_decimal_places": 0,  
-                           "color": BLACK, }, }, )
-        axes.y_axis.get_tick_marks()[0].set_opacity(0)
-        axes.y_axis.get_tick_marks()[3].set_opacity(0)
-        axes.y_axis.get_tick_marks()[4].set_opacity(0)
-        axes.x_axis.get_tick_marks()[0].set_opacity(0)
-        for num in axes.y_axis.numbers:
-          if num.get_value() == 1:
-           num.set_opacity(0)  
-          if num.get_value() < 0:
-            num.next_to( axes.y_axis.number_to_point(num.get_value()), RIGHT, buff=0.12)
+        number_line = NumberLine(
+            x_range=[-5, 5, 1],
+            length=10,
+            include_ticks=False,
+            include_numbers=False,
+            color=BLACK)
 
-        x_label = axes.get_x_axis_label(MathTex("x",color=BLACK)).shift(DOWN*0.3+RIGHT*0.07)
-        y_label = axes.get_y_axis_label(MathTex("y",color=BLACK)).shift(UP*0.2+LEFT*0.7)
-        line1 = axes.plot(lambda x: x - 1, x_range=[-2.5, 0], color="#26538D", stroke_width=5)
-        line2 = axes.plot(lambda x: x + 1, x_range=[0, 2.5], color="#A0025C", stroke_width=5)
-
-        circle1 = Circle(radius=0.05, color=BLACK,fill_color=WHITE, fill_opacity=1,stroke_width=1.5).move_to(axes.c2p(0, -1)).set_fill(opacity=1)
-        circle2 = Circle(radius=0.05, color=BLACK,fill_color=WHITE, fill_opacity=1,stroke_width=1.5).move_to(axes.c2p(0, 1)).set_fill(opacity=1)
-
-        label1 = MathTex("f(x)=x-1", color=BLACK).rotate(PI/4).move_to(axes.c2p(-2.5, -2.5)).scale(1).shift(UP*0.08+RIGHT*0.75)
-        label2 = MathTex("f(x)=x+1", color=BLACK).rotate(PI/4).move_to(axes.c2p(2.2, 2.7)).scale(1).shift(UP+LEFT*0.9+DOWN*0.8)
-        self.add(axes,x_label,y_label,line1, line2,circle1, circle2, label1, label2)
-
+        number_line.add_tip(tip_length=0.35,tip_width=0.25)
+        number_line.add_tip(at_start=True,tip_length=0.35,tip_width=0.25)
+        left_tip = StealthTip(color=BLACK).scale(0.9).move_to(number_line.n2p(0) + LEFT * 1).rotate(PI)
+        right_tip = StealthTip(color=BLACK).scale(0.9).move_to(number_line.n2p(0) + RIGHT * 1)
         
+        middle_line = Line(UP * 0.15,DOWN * 0.15,stroke_width=3,color=BLACK).move_to(number_line.n2p(0))
+        #labels
+        label1 = MathTex("0", color=BLACK).next_to(number_line.n2p(0), DOWN)
+        label2 = MathTex("0^-", color=BLACK).next_to(number_line.n2p(-1), UP)
+        label3 = MathTex("0^+", color=BLACK).next_to(number_line.n2p(1), UP)
+        label4 = MathTex("L.H.L", color=BLACK).next_to(label2, UP*1.4).shift(LEFT)
+        label5 = MathTex("R.H.L", color=BLACK).next_to(label3, UP*1).shift(RIGHT)
+        left_func = MathTex("f(x)=x-1", color=BLACK).next_to(number_line.n2p(-2.5), DOWN*2)
+        right_func = MathTex("f(x)=x+1", color=BLACK).next_to(number_line.n2p(2.5), DOWN*2)
+        #braces
+        left_brace = BraceBetweenPoints(
+            number_line.n2p(-4),
+            number_line.n2p(-1),
+            direction=DOWN,color=BLACK)
+
+        right_brace = BraceBetweenPoints(
+            number_line.n2p(1),
+            number_line.n2p(4),
+            direction=DOWN,color=BLACK)
+
+        self.add(
+            number_line,label1,label2,label3,label4,label5,
+            left_func,
+            right_func,
+            left_brace,
+            right_brace,  middle_line,left_tip, right_tip
+        )
