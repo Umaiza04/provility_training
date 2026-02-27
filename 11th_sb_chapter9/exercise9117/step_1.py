fe277ceaa6a16af3d16e78780982fcc9a4c0c56d
Umaiza04 <umaiza.cb2022@jerusalemengg.ac.in>
2026-02-27 16:13:27 +0530

    manim task commit

--- 11th_sb_chapter9/exercise9117/step_1.py
+++ 11th_sb_chapter9/exercise9117/step_1.py
@@ -1,4 +1,3 @@
-from turtle import fillcolor
 from manim import*
 config.frame_height=10
 config.frame_width=15
@@ -8,57 +7,48 @@ config.pixel_height=2500
 class Step_1(Scene):
     def construct(self):
         self.camera.background_color = WHITE
-        axes = Axes(
-            x_range=[-2.5*np.pi, 2.7*np.pi, np.pi/2],
-            y_range=[-4, 4, 1],
-            x_length=12,
-            y_length=7,
-            axis_config={"include_tip": True,
-                         "color": BLACK,
-                         "stroke_width": 3,"tip_length": 0.35,
-                         "tip_width": 0.25, },
-            y_axis_config={"numbers_to_include": [-1,-2,-3, 1, 2, 3],
-                           "decimal_number_config": {
-                           "num_decimal_places": 0,
-                           "color": BLACK, },  }, ) 
-        axes.x_axis.get_tick_marks()[9].set_opacity(0)
-        axes.y_axis.get_tick_marks()[0].set_opacity(0)
-        axes.x_axis.get_tick_marks()[0].set_opacity(0)
-        xticks = [ -2*np.pi, -3*np.pi/2, -np.pi, -np.pi/2, np.pi/2, np.pi, 3*np.pi/2, 2*np.pi ]
-        #labels creation
-        xtick_labels = [
-            MathTex("-2\\pi", color=BLACK),
-            MathTex("-\\frac{3\\pi}{2}", color=BLACK),
-            MathTex("-\\pi", color=BLACK),
-            MathTex("-\\frac{\\pi}{2}", color=BLACK),
-            MathTex("\\frac{\\pi}{2}", color=BLACK),
-            MathTex("\\pi", color=BLACK),
-            MathTex("\\frac{3\\pi}{2}", color=BLACK),
-            MathTex("2\\pi", color=BLACK),]
-        xlabels = VGroup()
-        for x, label in zip(xticks, xtick_labels):
-            label.scale(0.7)
-            label.next_to(axes.coords_to_point(x, 0), DOWN)
-            xlabels.add(label)
-        x_label = Text("x", color=BLACK).next_to(axes.x_axis.get_end(), RIGHT).scale(1)
-        y_label = Text("y", color=BLACK).next_to(axes.y_axis.get_end(), UP).scale(1)
-        #sin curve
-        sin_curve = axes.plot( lambda x: np.sin(x),x_range=[-2.1*np.pi, 0],color="#751973").insert_n_curves(100)
-        label1 = MathTex("f(x)=\\sin x", color=BLACK).next_to(axes.coords_to_point(-0.7*np.pi, -2.3), LEFT)
-        #1-cos(x)
-        p = 1.55
-        shift=0.2
-        cos_curve2 = axes.plot(lambda x: 2 * ((1 - np.cos(x+shift)) / 2) ** p, x_range=[0, np.pi],color="#D90166")
-        dot = Dot(axes.coords_to_point(np.pi, 2),color=BLACK)
-        label2 = MathTex("f(x)=1-\\cos x", color=BLACK).next_to(axes.coords_to_point(np.pi/2, 3), RIGHT)
-        #cos curve
-        x_shift = 0.08  
-        cos_curve = axes.plot(lambda x: np.cos(x),x_range=[np.pi + x_shift, 2.5*np.pi],color="#015482")
-        circle = Circle(radius=0.06, color=BLACK).move_to(axes.coords_to_point(np.pi, -1)).set_z_index(1)
-        label3 = MathTex("f(x)=\\cos x", color=BLACK).move_to(axes.coords_to_point(1.6*np.pi, -2.2)).shift(LEFT*0.5+UP*0.5)
-        #arrow creation
-        arrow1 = Arrow(start=LEFT*0.3,end=RIGHT * 0.1,  buff=0,stroke_width=3, tip_length=0.15,  color=BLACK).shift(DOWN*2.05+LEFT*1.7)
-        arrow2 = Arrow(start=RIGHT*0.3,end=LEFT * 0.1,  buff=0,stroke_width=3, tip_length=0.15,  color=BLACK).shift(DOWN*2.05+LEFT*5)
+        line = NumberLine(
+            x_range=[-5, 5, 1],
+            length=10,
+            include_ticks=False,
+            include_numbers=False,
+            color="#AC4F06",stroke_width=4)
+        line.add_tip(tip_length=0.35,tip_width=0.25)
+        line.add_tip(at_start=True,tip_length=0.35,tip_width=0.25)
+        zero = line.n2p(-1.7)
+        pi_point = line.n2p(1.5)
+        label1 = MathTex("-\\infty", color=BLACK).scale(0.9).next_to(line, LEFT).shift(DOWN*0.4+RIGHT*0.7)
+        label2 = MathTex("\\infty", color=BLACK).scale(0.9).next_to(line, RIGHT).shift(DOWN*0.4+LEFT*0.7)
+
+        # 0 and π labels
+        zero_label = MathTex("0", color=BLACK).scale(0.9).next_to(zero, DOWN)
+        pi_label = MathTex("\\pi", color=BLACK).scale(0.9).next_to(pi_point, DOWN).shift(DOWN*0.06)
+        tick_zero = Line( zero + UP*0.15, zero + DOWN*0.15, color=BLACK)
+        tick_pi = Line(pi_point + UP*0.15,pi_point + DOWN*0.15,color=BLACK)
         
-        self.add(sin_curve,cos_curve2,cos_curve,axes,x_label, y_label,xlabels,
-                 dot,circle,label1,label2,label3,arrow1,arrow2)
+        #braces
+        brace1 = BraceBetweenPoints(line.n2p(-4.8),line.n2p(-1.8),direction=DOWN,color=BLACK).shift(DOWN*0.4)
+        brace2 = BraceBetweenPoints(line.n2p(-1.6),line.n2p(1.4),direction=DOWN,color=BLACK).shift(DOWN*0.4)
+        brace3 = BraceBetweenPoints(line.n2p(1.6),line.n2p(4.8),direction=DOWN,color=BLACK).shift(DOWN*0.4)
+
+        #function labels
+        f1 = MathTex("f(x)=\\sin x", color=BLACK).scale(0.8).next_to(brace1, DOWN)
+        f2 = MathTex("f(x)=1-\\cos x", color=BLACK).scale(0.8).next_to(brace2, DOWN)
+        f3 = MathTex("f(x)=2\\cos x", color=BLACK).scale(0.8).next_to(brace3, DOWN)
+
+        #labels
+        shift_amt = 0.6
+        zero_minus = MathTex("0^{-}", color=BLACK).scale(0.8).move_to(zero + LEFT*shift_amt + UP*0.6)
+        zero_plus  = MathTex("0^{+}", color=BLACK).scale(0.8).move_to(zero + RIGHT*shift_amt + UP*0.6).shift(RIGHT*0.3)
+        pi_minus = MathTex("\\pi^{-}", color=BLACK).scale(0.8).move_to(pi_point + LEFT*shift_amt + UP*0.6)
+        pi_plus  = MathTex("\\pi^{+}", color=BLACK).scale(0.8).move_to(pi_point + RIGHT*shift_amt + UP*0.6).shift(RIGHT*0.3)
+
+        #arrows creation
+        height=0.6
+        arrow1 = Arrow(start=zero + LEFT*0.8 + UP*height,end=zero + LEFT*0.05 + UP*height,buff=0,stroke_width=2,color="#030AA7").shift(DOWN*0.3+LEFT*0.2)
+        arrow2 = Arrow(start=zero + RIGHT*0.8 + UP*height,end=zero + RIGHT*0.05 + UP*height,buff=0,stroke_width=2,color="#030AA7").shift(DOWN*0.3+RIGHT*0.2)
+        arrow3 = Arrow(start=pi_point + LEFT*0.8 + UP*height,end=pi_point + LEFT*0.05 + UP*height,buff=0,stroke_width=2,color="#030AA7").shift(DOWN*0.3+LEFT*0.2)
+        arrow4 = Arrow(start=pi_point + RIGHT*0.8 + UP*height,end=pi_point + RIGHT*0.05 + UP*height,buff=0,stroke_width=2,color="#030AA7").shift(DOWN*0.3+RIGHT*0.2)
+
+        self.add(line,label1,label2,zero_label, pi_label,brace1, brace2, brace3,f1, f2, f3,
+                 tick_zero,tick_pi,zero_minus,zero_plus,pi_minus,pi_plus,arrow1 ,arrow2,arrow3,arrow4)
